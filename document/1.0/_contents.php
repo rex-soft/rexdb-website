7e064126571d5946d88ef862ae4639913cee0396
Administrator <Administrator@DESKTOP-89AHBB8>
2016-08-30 18:16:09 +0800

    update

--- document/1.0/_contents.php
+++ document/1.0/_contents.php
@@ -2,7 +2,7 @@
 			<dt><a href="document.php?version=1.0&doc=intro">1. 简介 <span> - Rexdb框架的功能和特点</span></a></dt><dd></dd>
 			<dt><a href="document.php?version=1.0&doc=download">2. 下载 <span> - 如何获取Rexdb</span></a></dt><dd></dd>
 			<dt><a href="document.php?version=1.0&doc=quick-start-beginner">3. 快速入门（初学者） <span> - 面向Java编程初学者的快速入门教程</span></a></dt><dd></dd>
-			<dt><a href="document.php?version=1.0&doc=quick-start">4. 快速入门<span> - 面向初学者的快速入门教程</span></a></dt><dd></dd>
+			<dt><a href="document.php?version=1.0&doc=quick-start">4. 快速入门<span> - 面向多数编程人员的快速入门教程</span></a></dt><dd></dd>
 			<dt><a href="document.php?version=1.0&doc=user-manual">5. 用户手册 <span> - 完整的开发人员参考文档，包含详细的示例、配置和接口说明</span></a></dt><dd></dd>
 			<dt><a href="document.php?version=1.0&doc=user-manual-tester">6. 用户手册 - Rexdb性能测试程序 <span> - 性能测试程序的使用说明</span></a></dt><dd></dd>
 		</dl>
