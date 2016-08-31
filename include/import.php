955ed0bce62bead0c2a10a15aed5a359f44f1f52
Administrator <Administrator@DESKTOP-89AHBB8>
2016-08-31 16:59:09 +0800

    update

--- include/import.php
+++ include/import.php
@@ -1,6 +1,9 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,Chrome=1">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta content='java,开源,免费,框架,jdbc,持久层,OR,ORM,高性能,hibernate,mybatis,spring jdbc,查询数据库,国产' name='Keywords'>
+<meta content='Rexdb是一款全新的ORM框架，性能和开发效率均优于Hibernate、Mybatis和Spring JDBC，适用于互联网、金融、电子政务等各类场景' name='Description'>
+	
 <link rel="stylesheet" href="<?=$basePath?>ui/css/bootstrap.min-3.3.5.css" type="text/css"></link>
 <link rel="stylesheet" href="<?=$basePath?>ui/css/main-1.0.2.css" type="text/css"></link>
 <script src="<?=$basePath?>ui/js/jquery.min-1.11.3.js"></script>
